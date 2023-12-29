417f05b968e4bd477028677ba87b2a6f4f205e35
marvinpjr <marvinpalmerjr@gmail.com>
2023-12-28 20:16:06 -0600

    added sqlite database, dbcontext, and domain entity activity

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -11,4 +17,19 @@ if (app.Environment.IsDevelopment()) {}
 app.UseAuthorization();
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during database migration.");
+}
+
 app.Run();
+
