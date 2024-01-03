613e7542adf1864a7f2440062e5602facbb728f5
marvinpjr <marvinpalmerjr@gmail.com>
2024-01-02 18:39:12 -0600

    working view to see details of activity

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,13 +1,7 @@
 ﻿using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Activities
 {
---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,29 @@
+import { Grid } from "semantic-ui-react";
+import { Activity } from "../../../app/layout/models/activity";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+
+interface Props {
+    activities: Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: ()=> void;
+}
+
+export default function ActivityDashboard({activities, 
+    selectActivity, selectedActivity, cancelSelectActivity}: Props){
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList activities={activities} selectActivity={selectActivity}/>
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {selectedActivity &&
+                    <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity}/>
+                }  
+                <ActivityForm />
+            </Grid.Column>
+        </Grid>
+    )
+}
