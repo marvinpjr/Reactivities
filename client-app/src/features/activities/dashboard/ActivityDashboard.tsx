e7421d0621d048324adc64349c78e1b810f14321
marvinpjr <marvinpalmerjr@gmail.com>
2024-01-02 19:48:57 -0600

    working edit within client

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -9,20 +9,30 @@ interface Props {
     selectedActivity: Activity | undefined;
     selectActivity: (id: string) => void;
     cancelSelectActivity: ()=> void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
 }
 
 export default function ActivityDashboard({activities, 
-    selectActivity, selectedActivity, cancelSelectActivity}: Props){
+    selectActivity, selectedActivity, cancelSelectActivity, editMode, openForm, 
+    closeForm, createOrEdit}: Props){
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList activities={activities} selectActivity={selectActivity}/>
             </Grid.Column>
             <Grid.Column width='6'>
-                {selectedActivity &&
-                    <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity}/>
+                {selectedActivity && !editMode &&
+                    <ActivityDetails activity={selectedActivity} 
+                        cancelSelectActivity={cancelSelectActivity}
+                        openForm={openForm} 
+                    />
                 }  
-                <ActivityForm />
+                { editMode &&
+                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />
+                }                
             </Grid.Column>
         </Grid>
     )
